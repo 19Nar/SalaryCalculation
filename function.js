3da898390bd4095a9cf343bb9ef8fe90168cc273
19Nar <60644251+19Nar@users.noreply.github.com>
2020-03-03 14:27:44 +0100

    Update function.js

--- function.js
+++ function.js
@@ -8,7 +8,7 @@ Number() + Number
 var salaryPerHour = document.getElementById('txt_salary');
 var hoursPerMonth = document.getElementById('txt_month');
 var basicSalaryPerMonth = document.getElementById('txt_basic');
-var grossSalary = document.getElementById('txt_gross'); // result
+var Salary = document.getElementById('txt_gross'); // result
 var CalcButton = document.getElementById('calc');
 
 
@@ -20,7 +20,8 @@ function DoCalculation(){
 /*
 1h -15euro
 basicSalary = 289h*15
-grossSalary= basicsalary*21/100
+Salary after Taxes= basicsalary - basicsalary*21/100
+
 */
 /* 
 Changing 
@@ -30,7 +31,7 @@ Number() to Number
 var basicSalary = Number(hoursPerMonth.value) * Number(salaryPerHour.value);
 var SalarywithTax = basicSalary * 21/100;
 basicSalaryPerMonth.value=basicSalary;
-grossSalary.value= SalarywithTax;
+Salary.value= basicSalary - SalarywithTax;
 console.log(SalarywithTax);
 console.log(salaryPerHour.value);
 }
